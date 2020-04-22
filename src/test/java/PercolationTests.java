259638da1f7147f51fd833ad9d70c0c0e6396483
Jason Guarracino <gooeydev@gmail.com>
2020-04-22 16:33:30 +0000

    Working build

--- src/test/java/PercolationTests.java
+++ src/test/java/PercolationTests.java
@@ -1,7 +1,8 @@
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertFalse;
+import org.junit.jupiter.api.Test;
+
 
 public class PercolationTests {
 
